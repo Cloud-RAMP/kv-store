7638d1128ce0e64cdda361ed9aeaac4285f5a8d9
msoud2509 <msouders06@gmail.com>
2026-02-18 22:54:41 -0600

    add logging

--- internal/server/server_http.go
+++ internal/server/server_http.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
+	"time"
 	"github.com/Cloud-RAMP/kv-store/internal/store"
 )
 
@@ -32,10 +32,12 @@ func handleGet(w http.ResponseWriter, key string) {
 	val, err := store.Get(key)
 	if err != nil {
 		http.Error(w, "Key not found", 404)
+		logRequest(false, "GET", key)
 		return
 	}
 
 	w.Write([]byte(val))
+	logRequest(true, "GET", key)
 }
 
 func handlePut(w http.ResponseWriter, r *http.Request, key string) {
@@ -49,10 +51,16 @@ func handlePut(w http.ResponseWriter, r *http.Request, key string) {
 	}
 
 	store.Put(key, body.Value)
+	logRequest(true, "PUT", key)
 	w.WriteHeader(200)
 }
 
 func handleDel(w http.ResponseWriter, key string) {
 	store.Del(key)
+	logRequest(true, "DELETE", key)
 	w.WriteHeader(200)
+}
+
+func logRequest(success bool, method string, key string) {
+	fmt.Printf("Method:%s Key:%s Success:%t Time:%s\n", method, key, success, time.Now().Format("2006-01-02 15:04:05"))
 }
